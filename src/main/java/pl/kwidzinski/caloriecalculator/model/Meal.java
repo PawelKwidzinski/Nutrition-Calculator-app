fbb7ed75becf86117d2c9a89cdf7193569fef073
PawelKwidzinski <solvap83@gmail.com>
2021-04-12 18:54:15 +0200

    Created Meal class

---
+++ src/main/java/pl/kwidzinski/caloriecalculator/model/Meal.java
@@ -0,0 +1,99 @@
+package pl.kwidzinski.caloriecalculator.model;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Set;
+
+@Entity
+@Table(name = "meals")
+public class Meal {
+
+    @Id
+    @GeneratedValue(generator = "inc")
+    @GenericGenerator(name = "inc", strategy = "increment")
+    private Long id;
+
+    @NotNull(message = "Date cannot be empty")
+    @PastOrPresent(message = "Date cannot be from future")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate date;
+
+    private Integer totalKcal;
+    private BigDecimal totalProtein;
+    private BigDecimal totalFat;
+    private BigDecimal totalCarbs;
+    private BigDecimal totalFiber;
+
+    @OneToMany(mappedBy = "meal")
+    private Set<Ingredient> ingredients;
+
+     public Long getId() {
+        return id;
+    }
+
+     void setId(final Long id) {
+        this.id = id;
+    }
+
+     public LocalDate getDate() {
+        return date;
+    }
+
+     public void setDate(final LocalDate date) {
+        this.date = date;
+    }
+
+     public Integer getTotalKcal() {
+        return totalKcal;
+    }
+
+     public void setTotalKcal(final Integer totalKcal) {
+        this.totalKcal = totalKcal;
+    }
+
+     public BigDecimal getTotalProtein() {
+        return totalProtein;
+    }
+
+     public void setTotalProtein(final BigDecimal totalProtein) {
+        this.totalProtein = totalProtein;
+    }
+
+     public BigDecimal getTotalFat() {
+        return totalFat;
+    }
+
+     public void setTotalFat(final BigDecimal totalFat) {
+        this.totalFat = totalFat;
+    }
+
+     public BigDecimal getTotalCarbs() {
+        return totalCarbs;
+    }
+
+     public void setTotalCarbs(final BigDecimal totalCarbs) {
+        this.totalCarbs = totalCarbs;
+    }
+
+     public BigDecimal getTotalFiber() {
+        return totalFiber;
+    }
+
+     public void setTotalFiber(final BigDecimal totalFiber) {
+        this.totalFiber = totalFiber;
+    }
+
+     public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+     public void setIngredients(final Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+}
