4e13834096cfc2b368500393c29d4e42efeaf0d3
PawelKwidzinski <solvap83@gmail.com>
2021-04-16 17:18:08 +0200

    Added methods: getById(), removeMeal()

--- src/main/java/pl/kwidzinski/caloriecalculator/service/MealService.java
+++ src/main/java/pl/kwidzinski/caloriecalculator/service/MealService.java
@@ -2,10 +2,12 @@ package pl.kwidzinski.caloriecalculator.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pl.kwidzinski.caloriecalculator.exceptions.MealCannotDeleteException;
 import pl.kwidzinski.caloriecalculator.model.Meal;
 import pl.kwidzinski.caloriecalculator.repository.MealRepo;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MealService {
@@ -16,12 +18,27 @@ public class MealService {
         this.mealRepo = mealRepo;
     }
 
+    public Optional<Meal> getById(Long id) {
+        return mealRepo.findById(id);
+    }
+
     public List<Meal> findAll() {
         return mealRepo.findAll();
     }
 
     public void saveMeal(Meal meal) {
         mealRepo.save(meal);
+    }
+
+    public void removeMeal(Long id) {
+        if (mealRepo.existsById(id)) {
+            Meal mealToDelete = mealRepo.getOne(id);
+            if (mealToDelete.getIngredients().size() == 0) {
+                mealRepo.deleteById(id);
+            } else {
+                throw new MealCannotDeleteException(id);
+            }
+        }
     }
 }
 
