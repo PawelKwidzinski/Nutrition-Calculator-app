0157777bf6fcd4dc9968fac4814e12ea79879da1
PawelKwidzinski <solvap83@gmail.com>
2021-04-19 19:55:37 +0200

    Updated methods: addMeal()

--- src/main/java/pl/kwidzinski/caloriecalculator/controller/MealController.java
+++ src/main/java/pl/kwidzinski/caloriecalculator/controller/MealController.java
@@ -1,23 +1,25 @@
 package pl.kwidzinski.caloriecalculator.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.kwidzinski.caloriecalculator.model.Meal;
+import pl.kwidzinski.caloriecalculator.service.IngredientService;
 import pl.kwidzinski.caloriecalculator.service.MealService;
+
 
 
 @Controller
 @RequestMapping(path = "/meals/")
 public class MealController {
     private final MealService mealService;
+    private final IngredientService ingredientService;
 
-    @Autowired
-    public MealController(final MealService mealService) {
+    public MealController(final MealService mealService, final IngredientService ingredientService) {
         this.mealService = mealService;
+        this.ingredientService = ingredientService;
     }
 
     @GetMapping("/list")
@@ -29,12 +31,14 @@ public class MealController {
     @GetMapping("/add")
     public String addMeal(Model model) {
         model.addAttribute("meal", new Meal());
+        model.addAttribute("ingredients", ingredientService.getTempList());
         return "meal-form";
     }
 
     @PostMapping("/add")
-    public String addMeal(Meal meal){
+    public String addMeal(Meal meal) {
         mealService.saveMeal(meal);
+        ingredientService.getTempList().clear();
         return "redirect:/meals/list";
     }
 }
