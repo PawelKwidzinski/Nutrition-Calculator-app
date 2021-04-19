c2b416554d569bb127191cd2d11213b3039135a6
PawelKwidzinski <solvap83@gmail.com>
2021-04-19 20:02:16 +0200

    Added new methods: addIngredient(), editIngredient(), deleteIngredient(), deleteIngredientFromTempList()

--- src/main/java/pl/kwidzinski/caloriecalculator/controller/IngredientController.java
+++ src/main/java/pl/kwidzinski/caloriecalculator/controller/IngredientController.java
@@ -13,10 +13,13 @@ import pl.kwidzinski.caloriecalculator.model.Ingredient;
 import pl.kwidzinski.caloriecalculator.api.remotedata.DataFetcher;
 import pl.kwidzinski.caloriecalculator.dto.UserInput;
 import pl.kwidzinski.caloriecalculator.service.IngredientService;
+import pl.kwidzinski.caloriecalculator.service.MealService;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/ingredients/")
@@ -24,54 +27,70 @@ public class IngredientController {
 
     private final DataFetcher dataFetcher;
     private final IngredientService ingredientService;
+    private final MealService mealService;
     private List<Ingredient> ingredientsFromApi;
 
     @Autowired
-    public IngredientController(final DataFetcher dataFetcher, final IngredientService ingredientService) {
+    public IngredientController(final DataFetcher dataFetcher, final IngredientService ingredientService, final MealService mealService) {
         this.dataFetcher = dataFetcher;
         this.ingredientService = ingredientService;
+        this.mealService = mealService;
         this.ingredientsFromApi = new ArrayList<>();
     }
 
     @GetMapping("/search")
     public String searchIngredients(Model model) {
         model.addAttribute("userInput", new UserInput());
-        return "ingredient-form";
+        return "ingredient-search";
     }
 
     @PostMapping("/search")
     public String showSearchingIngredients(@Validated UserInput userInput, BindingResult result, Model model) {
         if (result.hasErrors()) {
-            return "ingredient-form";
+            return "ingredient-search";
         }
         try {
             int weight = Integer.parseInt(userInput.getWeight());
             if (weight <= 0) {
                 model.addAttribute("error", "Weight must be greater than zero!");
-                return "ingredient-form";
+                return "ingredient-search";
             }
 
             ingredientsFromApi = dataFetcher.fetchDataFromApi(userInput.getIngredientName(), weight);
 
             if (ingredientsFromApi.size() == 0) {
                 model.addAttribute("error", "Searching ingredient not found!");
-                return "ingredient-form";
+                return "ingredient-search";
             }
 
             model.addAttribute("foundIngredients", ingredientsFromApi);
             return "ingredient-list";
         } catch (NumberFormatException e) {
             model.addAttribute("error", "Weight must be a number!");
-            return "ingredient-form";
+            return "ingredient-search";
         }
     }
 
-    @GetMapping("/add/{id}")
-    public String saveIngredientFromApi(@PathVariable String id) {
+    @GetMapping("/add/{foodId}")
+    public String saveIngredientFromApi(@PathVariable String foodId) {
         Ingredient toSave = ingredientsFromApi.stream()
-                .filter(element -> Objects.equals(element.getFoodId(), id))
+                .filter(element -> Objects.equals(element.getFoodId(), foodId))
                 .findFirst().get();
         ingredientService.saveFromApi(toSave);
+        return "redirect:/ingredients/search";
+    }
+
+    @GetMapping("/add")
+    public String addIngredient(Model model) {
+        model.addAttribute("meals", mealService.findAll());
+        model.addAttribute("ingredient", new Ingredient());
+        return "ingredient-form";
+    }
+
+    @PostMapping("/add")
+    public String addIngredient(Ingredient ingredient, Long mealId) {
+        ingredient.setUnit("g");
+        ingredientService.saveIngredient(ingredient, mealId);
         return "redirect:/ingredients/list";
     }
 
@@ -80,5 +99,36 @@ public class IngredientController {
         List<Ingredient> allIngredients = ingredientService.findAll();
         model.addAttribute("foundIngredients", allIngredients);
         return "ingredient-saved-list";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editIngredient(Model model, @PathVariable(name = "id") Long id) {
+        Optional<Ingredient> optIngredient = ingredientService.findById(id);
+        if (optIngredient.isPresent()) {
+            model.addAttribute("meals", mealService.findAll());
+            model.addAttribute("ingredient", optIngredient.get());
+            return "ingredient-form";
+        }
+        return "ingredient-saved-list";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteIngredient(HttpServletRequest request, @PathVariable(name = "id") Long id) {
+        String referer = request.getHeader("referer");
+        ingredientService.deleteIngredient(id);
+        if (referer != null) {
+            return "redirect:" + referer;
+        }
+        return "redirect:/ingredients/list";
+    }
+
+    @GetMapping("/delete/{foodId}/{weight}")
+    public String deleteIngredientFromTempList(@PathVariable String foodId, @PathVariable Integer weight) {
+        Ingredient toDelete = ingredientService.getTempList().stream()
+                .filter(ingredient -> Objects.equals(ingredient.getFoodId(), foodId))
+                .filter(e -> Objects.equals(e.getQuantity(), weight))
+                .findFirst().get();
+        ingredientService.deleteFromTempList(toDelete);
+        return "redirect:/meals/add";
     }
 }
