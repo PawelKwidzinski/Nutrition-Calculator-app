397b7978727e228a3cd7067f6aee54c44dc452e0
PawelKwidzinski <solvap83@gmail.com>
2021-05-06 18:30:11 +0200

    Added method editMeal()

--- src/main/java/pl/kwidzinski/caloriecalculator/controller/MealController.java
+++ src/main/java/pl/kwidzinski/caloriecalculator/controller/MealController.java
@@ -5,10 +5,13 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.kwidzinski.caloriecalculator.model.Meal;
 import pl.kwidzinski.caloriecalculator.service.MealService;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping(path = "/meals/")
@@ -27,18 +30,29 @@ public class MealController {
 
     @GetMapping("/add")
     public String addMeal(Model model) {
-        model.addAttribute("meal", new Meal());
         model.addAttribute("ingredients", mealService.getTempList());
+        model.addAttribute("meal", new Meal());
         return "meal-form";
     }
 
     @PostMapping("/add")
-    public String addMeal(@Validated Meal meal, BindingResult result) {
-        if (result.hasErrors()){
+    public String addMeal(@Validated Meal meal, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+//            model.addAttribute("ingredients", mealService.getTempList());
             return "meal-form";
         }
         mealService.saveMeal(meal);
-        mealService.getTempList().clear();
         return "redirect:/meals/list";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editMeal(Model model, @PathVariable(name = "id") Long mealId) {
+        Optional<Meal> optionalMeal = mealService.findById(mealId);
+        if (optionalMeal.isPresent()) {
+            model.addAttribute("meal", optionalMeal.get());
+            model.addAttribute("ingredients", optionalMeal.get().getIngredients());
+            return "meal-form";
+        }
+        return "redirect:/meal/list";
     }
 }
