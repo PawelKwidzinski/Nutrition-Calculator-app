c48d5cb31e2a63464c51a5b384807c11b9e0ab6f
PawelKwidzinski <solvap83@gmail.com>
2021-05-07 18:36:02 +0200

    Added: method findAllByDateBetweenOrderByDateAsc()

--- src/main/java/pl/kwidzinski/caloriecalculator/repository/MealRepo.java
+++ src/main/java/pl/kwidzinski/caloriecalculator/repository/MealRepo.java
@@ -4,6 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.kwidzinski.caloriecalculator.model.Meal;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface MealRepo extends JpaRepository<Meal, Long> {
+
+    List<Meal> findAllByDateBetweenOrderByDateAsc(LocalDate from, LocalDate to);
 }
