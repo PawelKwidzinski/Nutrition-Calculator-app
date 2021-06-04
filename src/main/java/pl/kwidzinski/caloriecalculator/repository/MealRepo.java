281d6f321abcb956c836941146116fb28e0f16d7
PawelKwidzinski <solvap83@gmail.com>
2021-06-04 19:55:39 +0200

    Added method: findAllOrderByDate() with SQL native query

--- src/main/java/pl/kwidzinski/caloriecalculator/repository/MealRepo.java
+++ src/main/java/pl/kwidzinski/caloriecalculator/repository/MealRepo.java
@@ -1,6 +1,7 @@
 package pl.kwidzinski.caloriecalculator.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pl.kwidzinski.caloriecalculator.model.Meal;
 
@@ -11,4 +12,7 @@ import java.util.List;
 public interface MealRepo extends JpaRepository<Meal, Long> {
 
     List<Meal> findAllByDateBetweenOrderByDateAsc(LocalDate from, LocalDate to);
+
+    @Query(nativeQuery = true, value = "select * from meals order by date desc")
+    List<Meal> findAllOrderByDate();
 }
