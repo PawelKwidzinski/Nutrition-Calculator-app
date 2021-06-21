f914d4e5ab9672187de5bb3015f093a5b42fe7ae
PawelKwidzinski <solvap83@gmail.com>
2021-06-21 19:59:34 +0200

    Added test: should_add_given_Ingredient() and should_delete_Ingredient()

--- src/test/java/pl/kwidzinski/caloriecalculator/service/IngredientServiceTest.java
+++ src/test/java/pl/kwidzinski/caloriecalculator/service/IngredientServiceTest.java
@@ -8,7 +8,6 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import pl.kwidzinski.caloriecalculator.model.Ingredient;
 import pl.kwidzinski.caloriecalculator.repository.IngredientRepo;
-import pl.kwidzinski.caloriecalculator.repository.MealRepo;
 
 import java.util.Arrays;
 import java.util.List;
@@ -17,17 +16,13 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class IngredientServiceTest {
 
     @Mock
     private IngredientRepo ingredientRepo;
-
-    @Mock
-    private MealRepo mealRepo;
 
     @InjectMocks
     private IngredientService ingredientService;
@@ -70,5 +65,28 @@ class IngredientServiceTest {
         Optional<Ingredient> actual = ingredientService.findById(3L);
         Assertions.assertEquals(3L, actual.get().getId());
         verify(ingredientRepo).findById(ingredient3.getId());
+    }
+
+    @Test
+    void should_add_given_Ingredient() {
+        // given
+        Ingredient ingredient4 = new Ingredient();
+        ingredient4.setId(4L);
+        ingredient4.setName("Cheese");
+
+        // when
+        when(ingredientRepo.save(any(Ingredient.class))).thenReturn(ingredient4);
+
+        // then
+        ingredientService.saveIngredient(ingredient4);
+        verify(ingredientRepo).save(ingredient4);
+    }
+    @Test
+    void should_delete_Ingredient() {
+        // when
+        ingredientService.deleteIngredientById(1L);
+
+        //then
+        verify(ingredientRepo, times(1)).deleteById(anyLong());
     }
 }
