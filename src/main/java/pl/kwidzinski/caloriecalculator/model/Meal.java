da82935dc94ac1f2c8d0ab67712228ee3d641ddd
PawelKwidzinski <solvap83@gmail.com>
2021-10-18 09:51:55 +0200

    Minor changes in imports

--- src/main/java/pl/kwidzinski/caloriecalculator/model/Meal.java
+++ src/main/java/pl/kwidzinski/caloriecalculator/model/Meal.java
@@ -3,7 +3,12 @@ package pl.kwidzinski.caloriecalculator.model;
 import org.hibernate.annotations.GenericGenerator;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.ManyToMany;
+import javax.persistence.FetchType;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PastOrPresent;
