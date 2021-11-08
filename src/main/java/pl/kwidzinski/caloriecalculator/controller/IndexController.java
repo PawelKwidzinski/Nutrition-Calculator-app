451edd2107982e0d18acbb1de31f15d00c18b46a
PawelKwidzinski <solvap83@gmail.com>
2021-11-08 15:51:27 +0100

    Add endpoints for registration

--- src/main/java/pl/kwidzinski/caloriecalculator/controller/IndexController.java
+++ src/main/java/pl/kwidzinski/caloriecalculator/controller/IndexController.java
@@ -1,12 +1,27 @@
 package pl.kwidzinski.caloriecalculator.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.kwidzinski.caloriecalculator.dto.UserRegistration;
+import pl.kwidzinski.caloriecalculator.service.AccountService;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/")
 public class IndexController {
+
+    private final AccountService accountService;
+
+    @Autowired
+    public IndexController(final AccountService accountService) {
+        this.accountService = accountService;
+    }
 
     @GetMapping("/")
     public String index() {
@@ -16,5 +31,27 @@ public class IndexController {
     @GetMapping("/login")
     public String loginForm() {
         return "login-form";
+    }
+
+    @GetMapping("/register")
+    public String registerForm() {
+        return "register-form";
+    }
+
+    @PostMapping("/register")
+    public String register(@Valid UserRegistration userInput, BindingResult result, Model model) {
+        if (!userInput.arePasswordEqual()) {
+            model.addAttribute("errorMessage", "Passwords do not match");
+            return "register-form";
+        }
+        if (result.hasGlobalErrors()) {
+            model.addAttribute("errorMessage", result.getFieldError().getDefaultMessage());
+            return "register-form";
+        }
+        if (!accountService.register(userInput)) {
+            model.addAttribute("errorMessage", "This username is already taken.");
+            return "register-form";
+        }
+        return "redirect:/login";
     }
 }
